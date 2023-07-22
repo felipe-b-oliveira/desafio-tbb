ed0088aa57c9fce8ab975ceebd56175ee411d370
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-07-22 06:56:35 -0300

    feat: Add initial structure

---
+++ src/app/layout.tsx
@@ -0,0 +1,56 @@
+import '../styles/global.scss'
+import type { Metadata } from 'next'
+import { Roboto } from 'next/font/google'
+import { siteConfig } from "../config/site"
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: ["400", "500", "700"]
+})
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
+
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.title,
+    template: `%s | ${siteConfig.title}`
+  },
+  description: siteConfig.description,
+  keywords: [
+    "TBB",
+    "Store",
+  ],
+  authors: [
+    {
+      name: "felipeboliveira",
+      url: "https://felipe-b-oliveira.github.io/",
+    },
+  ],
+  creator: "felipeboliveira",
+  openGraph: {
+    type: "website",
+    locale: "pt_BR",
+    url: siteConfig.url,
+    title: siteConfig.title,
+    description: siteConfig.description,
+    siteName: siteConfig.title,
+  },
+  icons: {
+    icon: "/favicon.svg",
+    shortcut: "/favicon-16x16.svg",
+    apple: "/apple-touch-icon.svg",
+  },
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="pt-BR" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href="/favicon/favicon.svg" type="image/svg" />
+      </head>
+      <body className={roboto.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/config/site.ts
@@ -0,0 +1,11 @@
+import { SiteConfig } from "../types"
+
+export const siteConfig: SiteConfig = {
+    title: "TBB Store",
+    description: "An ecommerce application page built with Next.js 13 as part of the TBB selection process.",
+    url: "",
+    ogImage: "",
+    links: {
+        github: "https://github.com/felipe-b-oliveira/desafio-tbb",
+    },
+}
