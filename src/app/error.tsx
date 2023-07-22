447919bb691cf5ed7aba50c256b09ebb27fa7430
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-07-22 16:53:14 -0300

    feat: Add doc Error example

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,30 @@
 "use client"
 
-export default function Error() {
-    return <p>Ops, problemas tecnicos...</p>
+import { useEffect } from 'react'
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    useEffect(() => {
+        // Log the error to an error reporting service
+        console.error(error)
+    }, [error])
+
+    return (
+        <div>
+            <h2>Ops, Something went wrong!</h2>
+            <button
+                onClick={
+                    // Attempt to recover by trying to re-render the segment
+                    () => reset()
+                }
+            >
+                Try again
+            </button>
+        </div>
+    )
 }
