53db74e1c6b13f0fd7b55538214640a816817f78
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-07-23 07:57:23 -0300

    refactor: rename scss file to match component name

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import styles from './styles.module.scss';
+import styles from './header.module.scss';
 import { PrimaryInputWSearchIcon } from './Searchbar/primary-input';
 
 interface HeaderProps {
