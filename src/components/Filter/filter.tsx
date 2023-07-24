4f9dff00079593f69f8d172690d0c6b737d93cbe
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-07-24 03:34:42 -0300

    feat: add FilterPanel component

--- src/components/Filter/filter.tsx
+++ src/components/Filter/filter.tsx
@@ -1,7 +1,12 @@
+'use client'
+
+import { useFilter } from '@/hooks/useFilter';
 import styles from './filter.module.scss';
-import { FilterItem } from './filterItem';
+import { FilterItem } from './filter-item';
 
 export function FilterPanel() {
+    const { category, setCategory } = useFilter();
+
     return (
         <div className={styles.categoryFilter}>
             <summary className={styles.filterTitle} data-ic-title="filtros-categoria" data-testid="summaryFilter">Filtros</summary>
