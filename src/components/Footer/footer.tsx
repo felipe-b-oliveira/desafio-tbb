1654ae223e47f9c8ff279cf21a7f2d66641b3ee1
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-07-24 03:36:46 -0300

    style(footer): change update text description and size

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -5,7 +5,7 @@ export function Footer() {
     return (
         <div className={styles.footerContainer}>
             <div>
-                <Link href="https://github.com/felipe-b-oliveira/">© Copyright 2020 - Felipe Oliveira</Link>
+                <Link href="https://github.com/felipe-b-oliveira/">© Copyright 2023 - Felipe Oliveira</Link>
             </div>
         </div>
     )
